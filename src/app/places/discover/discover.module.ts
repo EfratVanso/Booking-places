b78f6f6c9f574ddd18f42f8e0b39f7b7f8996657
efiVanso <efi.vanso@gmail.com>
2020-06-25 20:59:04 +0300

    add routerLink to places dispayed

--- src/app/places/discover/discover.module.ts
+++ src/app/places/discover/discover.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+// import { Router } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { DiscoverPageRoutingModule } from './discover-routing.module';
 
 import { DiscoverPage } from './discover.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
+    // Router,
+    RouterModule,
     CommonModule,
     FormsModule,
     IonicModule,
