bac0f1b519ffe072f22c2479d70ac690afb6aecc
efiVanso <efi.vanso@gmail.com>
2020-07-02 19:08:34 +0300

    setting theme & global style

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -31,7 +31,7 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places/tabs/discover');
         loadingEl.dismiss();
         this.isLoading = false;
-      },3000);
+      },1500);
 
     });
 
